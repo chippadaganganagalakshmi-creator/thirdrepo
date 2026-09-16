fd51070dcde4baa1b9e096825cfe851a1c83184c
ganga <root@ip-172-31-46-122.eu-north-1.compute.internal>
2026-09-16 08:38:58 +0000

    data added

---
+++ application.py
@@ -0,0 +1,11 @@
+# Sample Python Program
+
+def greet(name):
+    return f"Hello, {name}!"
+
+# User input
+name = input("Enter your name: ")
+
+# Call function and print result
+message = greet(name)
+print(message)
